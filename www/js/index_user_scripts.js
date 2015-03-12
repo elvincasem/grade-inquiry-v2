e9b34461fcc541c7dae2eadeafb49d8e562aa165
elvincasem <elvin.casem@gmail.com>
2015-03-12 21:45:35 +0800

    working copy
    edited display of grades

--- www/js/index_user_scripts.js
+++ www/js/index_user_scripts.js
@@ -8,36 +8,18 @@
  {
     
     
-     /* button  #search */
-    
-    
-        /* button  #search */
-    
-    
-        /* button  #back */
-    
-    
         /* button  #back */
     $(document).on("click", "#back", function(evt)
     {
          activate_page("#mainpage"); 
     });
-    
-        /* button  #search */
-    
-    
-        /* button  #search */
-    $(document).on("click", "#search", function(evt)
-    {
-        /* your code goes here */ 
-    });
-    
+      
         /* button  #search */
     $(document).on("click", "#search", function(evt)
     {
         //
         $.ajax({
-                            url: 'http://localhost/github/graderesult/search.php',
+                            url: '  http://localhost/github/graderesult/search.php',
                             type: 'get',
                             data: {name: $('input#lastname').val(), idnumber: $('input#idnumber').val()},
                             success: function(response) {
@@ -45,9 +27,8 @@
                                 console.log(response);
                             }
                         });
-        //console.log($.ajax);
-        
-        activate_page("#grade-result"); 
+         activate_page("#grade-result"); 
+
     });
     
     }
